f4c6b85d8e50813bdb3a534abd51a100934ad650
Romanou <10Juin2013>
2017-05-18 16:22:53 +0200

    home

--- database/migrations/2017_05_18_141729_likes_table.php
+++ database/migrations/2017_05_18_141729_likes_table.php
@@ -13,7 +13,12 @@ class LikesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('song_id');
+            $table->timestamps();
+        });
     }
 
     /**
