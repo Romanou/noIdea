06da37cfe2cf45a6edb4e3c88d3a1ba8c0ea7585
Romanou <romandu62@hotmail.fr>
2017-06-02 17:43:36 +0200

    css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="UTF-8"/>
         <title>NoIedea - @yield('title')</title>
+        <link rel="stylesheet" href="/css/font-awesome.min.css">
         <link rel="stylesheet" href="/css/grid.css">
         <link rel="stylesheet" href="/css/style.css"/>
     </head>
@@ -10,6 +11,9 @@
             <ul>
                 <li><a href="/">Accueil</a></li>
                 @if(!Auth::check())
+                    <li id="searchButton">
+                        Rechercher
+                    </li>
                     <li><a href="/login">Connexion</a></li>
                 @else
                     <li>
@@ -26,7 +30,7 @@
             <div class="search">
                 <form action="/search" method="GET">
                     <input type="search" name="term" placeholder="Une recherche ?" required>
-                    <input type="submit">
+                    <button type="submit"><i class="fa fa-search"></i></button>
                 </form>
             </div>
         </nav>
