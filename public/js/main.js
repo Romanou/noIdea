fd99f379de3ce9055d95459140145c7d5e1da139
Romanou <romandu62@hotmail.fr>
2017-06-08 15:04:10 +0200

    css

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     document.querySelector('#searchButton').addEventListener('click',function () {
+        this.classList.toggle('open');
         document.querySelector('div.search').classList.toggle('open');
     });
 }
