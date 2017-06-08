d3f0d2875521cd3b97428e5d0482449a306021b7
Romanou <romandu62@hotmail.fr>
2017-06-08 17:28:16 +0200

    css

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,8 @@ window.onload = function () {
         this.classList.toggle('open');
         document.querySelector('div.search').classList.toggle('open');
     });
-}
+
+    document.querySelector('#header div.menu div.hamb').addEventListener('click',function () {
+        document.querySelector('#header').classList.toggle('open');
+    });
+};
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <title>NoIedea - @yield('title')</title>
         <link rel="stylesheet" href="/css/font-awesome.min.css">
         <link rel="stylesheet" href="/css/grid.css">
@@ -22,7 +23,7 @@
                 <ul>
                     <li><a href="/">Accueil</a></li>
                     @if(!Auth::check())
-                        <li id="searchButton">
+                        <li id="searchButton" class="desk">
                             Rechercher
                         </li>
                         <li><a href="/login">Connexion</a></li>
@@ -30,16 +31,22 @@
                         <li>
                             <a href="/song/new">Nouveau son</a>
                         </li>
-                        <li id="searchButton">
+                        <li id="searchButton" class="desk">
                             Rechercher
                         </li>
                         <li>
                             <a href="/logout">Deconnexion</a>
                         </li>
                     @endif
+                        <li class="search mob">
+                            <form action="/search" method="GET">
+                                <input type="search" name="term" placeholder="Une recherche ?" required>
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </li>
                 </ul>
             </div>
-            <div class="search">
+            <div class="search desk">
                 <form action="/search" method="GET">
                     <input type="search" name="term" placeholder="Une recherche ?" required>
                     <button type="submit"><i class="fa fa-search"></i></button>
